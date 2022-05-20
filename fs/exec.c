be1fd51f4eeeec52c91be78e276ed643c3d7ce87
Lex4r <axelfacundonunezvera@gmail.com>
2022-05-20 14:57:18 +0000

    Fix build errors

--- fs/exec.c
+++ fs/exec.c
@@ -1070,9 +1070,8 @@ killed:
 
 char *__get_task_comm(char *buf, size_t buf_size, struct task_struct *tsk)
 {
-	/* buf must be at least sizeof(*(tsk->comm)) in size */
 	task_lock(tsk);
-	strncpy(buf, tsk->comm, sizeof(*(buf_size)));
+	strncpy(buf, tsk->comm, buf_size);
 	task_unlock(tsk);
 	return buf;
 }
